903a8ebc24c42e57afde4c00478301c8222483ad
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-07-30 13:36:25 -0600

    commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let twixBar = {
+    fillings: [
+        { outerLayer: "gold wrapper" },
+{ firstLayer: "milkChocolate" },
+{ middleLayer: "caramel" },
+{ bottomLayer: "cookie" },
+    ] 
+}
+
+console.log(twixBar)
