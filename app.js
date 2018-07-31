5632c763fb02fb6b9a44e171d944c66d7075f173
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-07-31 09:20:26 -0600

    commit

--- app.js
+++ app.js
@@ -1,10 +1,56 @@
 let twixBar = {
-    fillings: [
-        { outerLayer: "gold wrapper" },
-{ firstLayer: "milkChocolate" },
-{ middleLayer: "caramel" },
-{ bottomLayer: "cookie" },
-    ] 
+    fillings: {
+         outerLayer: "goldWrapper" ,
+         firstLayer: "milkChocolate" ,
+         middleLayer: "caramel" ,
+         bottomLayer: "cookie" ,
+    }
 }
 
-console.log(twixBar)
+
+
+let fruitSnacks = {
+    gummys: [
+       
+     "Strawberry",
+       "Orange" ,
+       "Grape" ,
+       "Raspberry" ,
+       "Green Apple",
+    ]
+}
+
+let granolaBar = {
+    color: "brown",
+    ingredients: [
+        
+        "granola" ,
+        "Brown Sugar",
+        "Brown Crisp Rice",
+        "Sugar",
+        "Dried Coconut",
+        "Soybean Oil",
+        "Semi Sweet Chocolate Chips",
+        "Vanilla Extract" ,
+        "Cocoa Butter" ,
+        
+    ]
+}
+
+let laptop = {
+    hdd: {
+        space: 256,
+        brand:"Seagate"
+    },
+    keyboard: steelSeries,
+
+    processor: {
+        speed: 2.80,
+        brand: "Intel Core i7"
+    },
+}
+
+let steelSeries = {
+    brand: "steel Series",
+    color:"rainbow"
+}
